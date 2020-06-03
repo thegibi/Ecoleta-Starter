8946ec45924ceb5caa935a037825bfcbb8dd9f6b
EvandroGibicoski <evandrogibicoski@outlook.com>
2020-06-03 13:03:52 -0300

    add image-template

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -15,8 +15,13 @@ populateUFs()
 
 function getCities(event)  {
     const citySelect = document.querySelector("select[name=city")
+    const stateInput = document.querySelector("input[name=state")
+
 
     const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
