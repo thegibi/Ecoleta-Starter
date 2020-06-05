8ade7672219cb1e64342b01e85692629c6ecbf90
EvandroGibicoski <evandrogibicoski@outlook.com>
2020-06-04 21:51:53 -0300

    add gitignore

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
 server.use(express.static("public"));
 
+
+nunjucks.configure("src/views", {
+    express:server,
+    autoescape: false,
+    noCache: true
+});
+
 server.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
+    return res.render("index.html", { })
+});
 server.get("/create-point", (req, res) => {
-    return res.sendFile(__dirname + "/views/create-point.html")
-})
+    return res.render("create-point.html")
+});
 server.listen(3000, () => {
     console.log("Server is runnig")
 });
