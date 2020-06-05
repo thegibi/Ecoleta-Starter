64218aaef7951129b377facfa3cfc6b316ef1bbe
EvandroGibicoski <evandrogibicoski@outlook.com>
2020-06-05 00:22:24 -0300

    ok

--- src/server.js
+++ src/server.js
@@ -13,10 +13,13 @@ nunjucks.configure("src/views", {
 });
 
 server.get("/", (req, res) => {
-    return res.render("index.html", { })
+    return res.render("index.html")
 });
 server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
+});
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
 });
 server.listen(3000, () => {
     console.log("Server is runnig")
